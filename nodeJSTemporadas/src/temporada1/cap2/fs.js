df59787dea35cca5761030eb4bb4ca4a430160b4
andersonrev <fabiander_21@hotmail.es>
2021-06-09 16:02:01 -0500

    once callbacks

--- nodeJSTemporadas/src/temporada1/cap2/fs.js
+++ nodeJSTemporadas/src/temporada1/cap2/fs.js
@@ -2,36 +2,48 @@
 
 const fs = require('fs');
 const path = require('path');
- 
+const once = require('once');
+
 
 // funcion sincrona
-const file = fs.readFileSync('callback.js','utf-8');
-console.log(file);
+// const file = fs.readFileSync('callback.js','utf-8');
+// console.log(file);
 
 // funcion asincrona
-fs.readFile('callback.js', 'utf-8', onReadFile );
 
 
-// function Expresion
-const onWriteFile = err => {
+
+function doFileOperations (filename, callback){
+
+  callback = once(callback);
+
+  fs.readFile(filename, 'utf-8', onReadFile );
+  // function Expresion
+  const onWriteFile = err => {
     if (err){
       console.log(err.message);
       return;
     }
-     console.log('listo !!!');
+    console.log('listo !!!');
+    callback();
   }
 
 
-function onReadFile(err,data){
-  if(err){
-    console.log(err);
-    return;
-  } 
-   const modified = data.replace(/setTimeout/g, 'setInterval');
-  fs.writeFile('callback_interval.js', modified, onWriteFile)
-  // console.log(data);
+  function onReadFile(err,data){ 
+    if(err){
+      console.log(err);
+      return;
+    } 
+    const modified = data.replace(/setTimeout/g, 'setInterval');
+    fs.writeFile('callback_interval.js', modified, onWriteFile)
+    // console.log(data);
+    callback();
+  }
 }
 
+doFileOperations('callback.js', ()=>{
+  console.log('hemos terminado todo!!');
+});
 /*
  * Best Practices
  * ------------------------------
