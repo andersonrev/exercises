148ff3454b0fbadfc9b94ec9287370f31cf7c6fc
andersonrev <fabiander_21@hotmail.es>
2021-06-09 15:46:47 -0500

    function expresion

--- nodeJSTemporadas/src/temporada1/cap2/fs.js
+++ nodeJSTemporadas/src/temporada1/cap2/fs.js
@@ -11,20 +11,25 @@ console.log(file);
 // funcion asincrona
 fs.readFile('callback.js', 'utf-8', onReadFile );
 
+
+// function Expresion
+const onWriteFile = err => {
+    if (err){
+      console.log(err.message);
+      return;
+    }
+     console.log('listo !!!');
+  }
+
+
 function onReadFile(err,data){
   if(err){
     console.log(err);
     return;
   } 
    const modified = data.replace(/setTimeout/g, 'setInterval');
-  fs.writeFile('callback_interval.js', modified, err => {
-    if (err){
-      console.log(err.message);
-      return;
-    }
-     console.log('listo !!!');
-  })
-  console.log(data);
+  fs.writeFile('callback_interval.js', modified, onWriteFile)
+  // console.log(data);
 }
 
 /*
