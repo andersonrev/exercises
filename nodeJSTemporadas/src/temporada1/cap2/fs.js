4bbf38a24b993859e77e1a1df33c1f27a709521d
andersonrev <fabiander_21@hotmail.es>
2021-06-09 15:42:25 -0500

    callback_ hell !!

--- nodeJSTemporadas/src/temporada1/cap2/fs.js
+++ nodeJSTemporadas/src/temporada1/cap2/fs.js
@@ -1,10 +1,34 @@
 'use strict'
 
 const fs = require('fs');
+const path = require('path');
+ 
 
+// funcion sincrona
 const file = fs.readFileSync('callback.js','utf-8');
 console.log(file);
 
-fs.readFile('callback.js', 'utf-8', function(err,data){
-  
-})
+// funcion asincrona
+fs.readFile('callback.js', 'utf-8', onReadFile );
+
+function onReadFile(err,data){
+  if(err){
+    console.log(err);
+    return;
+  } 
+   const modified = data.replace(/setTimeout/g, 'setInterval');
+  fs.writeFile('callback_interval.js', modified, err => {
+    if (err){
+      console.log(err.message);
+      return;
+    }
+     console.log('listo !!!');
+  })
+  console.log(data);
+}
+
+/*
+ * Best Practices
+ * ------------------------------
+ *  todas las funciones anónimas tienen un nombre.
+ */
