76b2d39a96423ffc8b1719b6703466538d3a2b05
andersonrev <fabiander_21@hotmail.es>
2021-06-11 16:57:49 -0500

    promise

--- nodeJSTemporadas/src/temporada1/cap3/promise.js
+++ nodeJSTemporadas/src/temporada1/cap3/promise.js
@@ -1,19 +1,50 @@
 'use strict'
 
-const fs = require('fs')
+const fs = require('fs').promises
+const { promisify, callbackify} = require('util');
 
 console.log('start script');
 
-function readFile(file) {
-    const p = new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            resolve('this is a file')
-        },500)
+/*function readFile(file) {
+    const p = new Promise((resolve, reject) => {
+        fs.readFile(file, 'utf8', (err, file) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(file);
+        })
     })
     return p;
-}
+}*/
 
-readFile().then().catch()
+
+fs.readFile(__filename, 'utf8')
+    .then(file => {
+        console.log(file);
+    }
+    ).catch(
+        err => {
+            console.log(err)
+        }
+    )
+
+
+Promise.all([
+    fs.readFile(__filename),
+    fs.readFile('gs,js'),
+    fs.readFile('afadfa')
+])
+.then(
+    values => {
+        console.log(values);
+    }
+)
+
+process.on('uncaughtException', err => {
+    console.log(err)
+    process.exit(1)
+})
 /*
 fs.readFile(__filename, 'utf8', (err, file) => {
     if(err){
