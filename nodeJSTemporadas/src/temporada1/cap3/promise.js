e39db0eb9ee9d2683773582543da69f64aaa6438
andersonrev <fabiander_21@hotmail.es>
2021-06-18 17:00:30 -0500

    promesas all and race and promise en import

--- nodeJSTemporadas/src/temporada1/cap3/promise.js
+++ nodeJSTemporadas/src/temporada1/cap3/promise.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const fs = require('fs').promises
+const fs = require('fs')
 const { promisify, callbackify} = require('util');
 
 console.log('start script');
 
-/*function readFile(file) {
+function readFile(file) {
     const p = new Promise((resolve, reject) => {
         fs.readFile(file, 'utf8', (err, file) => {
             if (err) {
@@ -16,34 +16,54 @@ console.log('start script');
         })
     })
     return p;
-}*/
+}
 
-
-fs.readFile(__filename, 'utf8')
+/*fs.readFile(__filename, 'utf8')
     .then(file => {
+        console.log('que mas ve');
         console.log(file);
     }
     ).catch(
         err => {
-            console.log(err)
+            console.log('xdxdxd',err)
         }
     )
-
-
+*/
+const p1 = readFile(__filename);
+// const p2 = readFile('fs.js');
 Promise.all([
-    fs.readFile(__filename),
+    /*fs.readFile(__filename),
     fs.readFile('gs,js'),
-    fs.readFile('afadfa')
+    */
+   p1,
+  // p1
+
 ])
 .then(
     values => {
         console.log(values);
     }
-)
+).catch(err => {
+    console.log('error en el ultimo catch,',err);
+});
+
+/*Promise.race([
+    fs.readFile(__filename),
+])
+.then(
+    values => {
+        console.log(values)
+    }
+)*/
 
 process.on('uncaughtException', err => {
     console.log(err)
     process.exit(1)
+})
+
+process.on('unhandledRejection', err => {
+    console.log(err);
+    process.exit(1);
 })
 /*
 fs.readFile(__filename, 'utf8', (err, file) => {
